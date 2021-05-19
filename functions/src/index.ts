278ef78742caad4d001e929e500a6bff97f4c3a6
Jesús Antonio Pérez Reyes <00antonio.perez00@gmail.com>
2021-05-19 12:13:48 -0500

    use express to manage main function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,27 @@
 import * as functions from "firebase-functions";
+import * as express from "express";
+import * as cors from "cors";
 
+const app = express();
+const v0 = express();
 
+// // Automatically allow cross-origin requests
+app.use(cors({origin: true}));
+//
+// // Add middleware to authenticate requests
+// app.use(myMiddleware);
+//
+// // build multiple CRUD interfaces:
+// app.get('/:id', (req, res) => res.send(Widgets.getById(req.params.id)));
+// app.post('/', (req, res) => res.send(Widgets.create()));
+// app.put('/:id',
+// (req, res) => res.send(Widgets.update(req.params.id, req.body)));
+// app.delete('/:id', (req, res) => res.send(Widgets.delete(req.params.id)));
+// app.get('/', (req, res) => res.send(Widgets.list()));
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-    functions.logger.info("Hello logs!", {structuredData: true});
-    response.send("Hello!");
+v0.get("/", function(req, res) {
+    res.send("Hello World");
 });
+
+app.use("/v0", v0);
+export const api = functions.https.onRequest(app);
